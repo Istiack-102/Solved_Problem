115628827455fe50a4a20a0bfab33e87bbb61c1d
Istiack <istiackr355355@gmail.com>
2026-06-18 09:59:05 +0600

    delete

--- Solved_Problem/Solved_Problem/emum.cpp
+++
@@ -1,8 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-enum season {spring,summer=12,winter};
-int main (){
-    season s;
-    s=summer;
-    cout<<s;
-}
--- Solved_Problem/Solved_Problem/sdd.cpp
+++
@@ -1,6 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main (){
-    string s ="\0";
-    cout<<s.size();
-}
